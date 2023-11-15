1d3ba7f2db0572a684fb94017cc7a0fe46d25342
Mahmoud Gamal <mgamal@sgs-uae.com>
2023-11-15 17:26:39 +0200

    Connecting to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB).then(() => {
+    console.log("Connected Successfully");
+}).catch((err) => {console.log("Could Not Connect")});
 
 const app = express();
-app.listen(3000 , () => {
-    console.log("App Is Running In Port 3000!!!")
+app.listen(9292 , () => {
+    console.log("App Is Running In Port 9292...!")
 })
