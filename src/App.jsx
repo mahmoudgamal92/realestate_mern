092867910ca6c599c16685c7d576e4a47b071adc
Mahmoud Gamal <mgamal@sgs-uae.com>
2023-11-15 14:27:16 +0200

    Configure Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Home from "./screens/Home";
+import SignIn from "./screens/SignIn";
+import SignUp from "./screens/SignUp";
+import About from "./screens/About";
+import Profile from './screens/Profile';
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element = {<Home/>}/>
+    <Route path='/SignIn' element = {<SignIn />}/>
+    <Route path='/SignUp' element = {<SignUp/>}/>
+    <Route path='/About' element = {<About/>}/>
+    <Route path='/Profile' element = {<Profile/>}/>
+  </Routes>
+  </BrowserRouter>
   )
 }
