b7f6bf7e3d1e632ebd9c64d1996ff41cc77442d1
Mahmoud Gamal <mgamal@sgs-uae.com>
2023-11-16 13:54:16 +0200

    Create Test API Routes

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,7 @@
+
+export const userController = (req,res) => {
+    res.send({
+        success : true,
+        message : "User Router From Controller",
+    });
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+import userRouter from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_DB).then(() => {
@@ -11,3 +14,6 @@ const app = express();
 app.listen(9292 , () => {
     console.log("App Is Running In Port 9292...!")
 })
+
+app.use("/api/user",userRouter);
+
